e1e42bcc0fefc54322ec26b80978829dca26337e
Igor Sergiienko <Igor_Sergiienko@epam.com>
2016-07-20 17:23:39 +0300

    carousel

---
+++ src/components/carousel/carousel.js
@@ -0,0 +1,8 @@
+﻿app.component('carousel', {
+	templateUrl: '../../components/carousel/carousel.html',
+	controller: 'carouselController'
+});
+
+app.controller('carouselController', function (model){
+	var $ctrl = this;
+});
